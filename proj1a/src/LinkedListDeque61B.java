f63108d35f43dfc44dcab29cae1a13d2daf70cf1
firas <firasnaber@gmail.com>
2025-06-29 19:57:44 -0500

    proj1a: Implemented the constructor for LinkedListDeque61B

---
+++ proj1a/src/LinkedListDeque61B.java
@@ -0,0 +1,66 @@
+import java.util.List;
+
+public class LinkedListDeque61B<T> implements Deque61B<T> {
+    private class Node {
+        public Node prev;
+        public T item;
+        public Node next;
+
+        public Node(Node prev, T item, Node next) {
+            this.prev = prev;
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+    private Node sentinel;
+
+    public LinkedListDeque61B() {
+        sentinel = new Node(null, null, null);
+    }
+
+    @Override
+    public void addFirst(T x) {
+
+    }
+
+    @Override
+    public void addLast(T x) {
+
+    }
+
+    @Override
+    public List<T> toList() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
