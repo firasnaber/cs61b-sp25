9583bd1ef1971c8c3a0a8658e9f5db6254b85fba
firas <firasnaber@gmail.com>
2025-07-06 22:37:50 -0500

    proj1b: Add skeleton code and implement addFirst and addLast methods and tests

---
+++ proj1b/src/deque/ArrayDeque61B.java
@@ -0,0 +1,80 @@
+package deque;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayDeque61B<T> implements Deque61B<T> {
+    private T[] array;
+    public int nextFirst; // TODO: change to default after testing
+    public int nextLast; // TODO: change to default after testing
+    int size;
+
+    public ArrayDeque61B() {
+        array = (T[]) new Object[8];;
+        nextFirst = 4;
+        nextLast = 5;
+        size = 0;
+    }
+
+    @Override
+    public void addFirst(T x) {
+        if (array[nextFirst] != null) {
+            // TODO: resizeArray();
+        }
+        array[nextFirst] = x;
+        nextFirst = Math.floorMod(nextFirst - 1, array.length);
+        size++;
+    }
+
+    @Override
+    public void addLast(T x) {
+        if (array[nextLast] == null) {
+            // TODO: resizeArray();
+        }
+        array[nextLast] = x;
+        nextLast = Math.floorMod(nextLast + 1, array.length);
+        size++;
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> returnList = new ArrayList<>();
+        int temp = nextFirst + 1;
+        for (int i = 0; i < array.length; i++) {
+            int curIndex = Math.floorMod(temp, array.length);
+            returnList.add(array[curIndex]);
+            temp++;
+        }
+        return returnList;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
--- proj1b/tests/ArrayDeque61BTest.java
+++ proj1b/tests/ArrayDeque61BTest.java
@@ -1,10 +1,13 @@
 import deque.ArrayDeque61B;
 
+import deque.Deque61B;
+import edu.princeton.cs.algs4.In;
 import jh61b.utils.Reflection;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -12,14 +15,51 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDeque61BTest {
 
-//     @Test
-//     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
-//     void noNonTrivialFields() {
-//         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
-//                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
-//                 .toList();
-//
-//         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
-//     }
+     @Test
+     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
+     void noNonTrivialFields() {
+         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
+                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
+                 .toList();
 
+         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+     }
+
+    @Test
+    public void addFirstTest() {
+         Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+         lld1.addFirst(5);
+         lld1.addFirst(4);
+         lld1.addFirst(3);
+         lld1.addFirst(2);
+         lld1.addFirst(1);
+
+         List<Integer> output = new ArrayList<>();
+
+         for (int i = 1; i <= 8; i++) {
+             if (i <= 5) output.add(i);
+             else output.add(null);
+         }
+
+         assertThat(lld1.toList()).isEqualTo(output);
+    }
+
+    @Test
+    void addLastTest() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        lld1.addLast(1);
+        lld1.addLast(2);
+        lld1.addLast(3);
+        lld1.addLast(4);
+        lld1.addLast(5);
+
+        List<Integer> output = new ArrayList<>();
+
+        for (int i = 1; i <= 8; i++) {
+            if (i <= 5) output.add(i);
+            else output.add(null);
+        }
+
+        assertThat(lld1.toList()).isEqualTo(output);
+    }
 }
